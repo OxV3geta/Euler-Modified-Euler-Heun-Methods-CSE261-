52be0248f2e14828b8ffd33b35b60ffb88a09b92
riyadzaman <riyadzaman014@gmail.com>
2026-01-14 22:47:24 +0600

    My code this finish
    Added the exact solution function for the test problem.

--- Include/ODEFunctions.h
+++ Include/ODEFunctions.h
@@ -18,3 +18,11 @@ namespace ODEProblems {
     inline double test_derivative(double t, double y) {
         return y - (t * t) + 1; 
     }
+
+ // The analytical (exact) solution: y(t) = (t+1)^2 - 0.5*e^t
+    inline double test_exact(double t) {
+        return ((t + 1) * (t + 1)) - (0.5 * std::exp(t));
+    }
+}
+
+#endif
