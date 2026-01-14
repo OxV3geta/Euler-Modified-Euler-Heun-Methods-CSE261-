5abbae1ef0cdedd35fd69e7e9122235869031b8f
NuraNusratJannat <nuranusratjannat@gmail.com>
2026-01-14 21:37:37 +0600

    Adding

--- code/main.cpp
+++ code/main.cpp
@@ -4,3 +4,7 @@
 #include "../include/NumericalMethods.h"
 #include "../include/FileWriter.h"
 #include "../include/ODEFunctions.h"
+
+int main() {
+    std::cout << "=== CSE261 Numerical Methods Project ===\n";
+    std::cout << "Method: Euler, Heun, Midpoint\n\n";
